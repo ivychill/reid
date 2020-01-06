6dc8a2ea21dfa8037d26a7184c86e2fb59e3ab9e
feng.chen <ivychill@163.com>
2020-01-06 17:39:02 +0800

    final

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ case $1 in
 
     trick)
         python test.py \
-            --gpu_ids 0 \
+            --gpu_ids 1 \
             --name warm5_s1_b8_lr2_p0.5 \
             --test_dir ../dataset/match/pytorch \
             --batchsize 32 \
@@ -12,37 +12,50 @@ case $1 in
         ;;
 
     pcb)
-        python main_test_arc.py \
-            --gpu_ids 0 \
+        python main_test.py \
+            --gpu_ids 1 \
             --PCB densenet \
             --stage pcb \
             --data_dir ../dataset/match/pytorch \
-            --model_dir ./model/base \
-            --result_dir ./result/base \
+            --model_dir ./model/812 \
+            --result_dir ./result/812_300 \
             --batchsize 256 \
-            --which_epoch best \
+            --which_epoch last \
             --scales 1,0.9
         ;;
 
     rpp)
         python main_test.py \
-            --gpu_ids 0 \
+            --gpu_ids 1 \
             --PCB densenet \
             --stage full \
             --RPP \
             --data_dir ../dataset/match/pytorch \
-            --model_dir ./model/base \
-            --result_dir ./result/base \
+            --model_dir ./model/erase \
+            --result_dir ./result/erase \
             --batchsize 256 \
-            --which_epoch best \
-            --scales 1,0.9
+            --which_epoch last \
+            --ms 1,0.9
         ;;
 
     rerank)
         python rerank_output.py \
-            --model_dir ./model/base \
-            --result_dir ./result/base \
+            --model_dir ./model/812 \
+            --result_dir ./result/812_300 \
             --data_dir ../dataset/match/pytorch
+        ;;
+
+    mmd)
+        python mmd.py \
+            --gpu_ids 1 \
+            --PCB densenet \
+            --stage pcb \
+            --data_dir ../dataset/match/pytorch \
+            --model_dir ./model/erase \
+            --result_dir ./result/mmd \
+            --batchsize 256 \
+            --which_epoch last \
+            --scale 1,0.9
         ;;
 
     *)
